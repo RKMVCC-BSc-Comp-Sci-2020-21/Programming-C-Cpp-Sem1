482305e943e0b70839996dd4af8247014f3c47b2
Srijan Dutta <srijandutta200@gmail.com>
2020-11-25 21:16:10 +0530

    Create VowelToUpperCase.cpp

---
+++ 25-11-2020/VowelToUpperCase.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+cout << "Enter the Sentence\n";
+string a;
+cin >> a;
+int x = a.length();
+	string c = "AEIOU";
+	for (int i = 0; i < x; i++) {
+		if ((a[i] == 'a') || (a[i] == 'e') || (a[i] == 'i') || (a[i] == 'o') || (a[i] == 'u'))
+			a[i] = a[i] - 'a' + 'A';
+	}
+cout << "The processed string is" << a;
+}
