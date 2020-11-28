3fbfb4c681ea29d226243af2f0fba7f2b3565cf2
Srijan Dutta <srijandutta200@gmail.com>
2020-11-28 10:29:56 +0530

    Create Calendar.cpp

---
+++ 27-11-2020/Calendar.cpp
@@ -0,0 +1,19 @@
+//Write a Program to enter the number of days and convert it to years and weeks
+
+#include <iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(void) {
+	int n, i;
+	cout << "Enter the number of days:\n";
+	cin >> n;
+	i = n;
+	int y, w;
+	if (n > 365)
+		y = n / 365;
+	
+	w = n / 7;
+	cout << "Converted Date:\n Years:" << y << "\nWeeks:" << w;
+}
