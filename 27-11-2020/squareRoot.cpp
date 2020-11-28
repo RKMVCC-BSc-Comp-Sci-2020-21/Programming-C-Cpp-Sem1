ade7772c8d9fcd5c4a172a5e2618c7b70b179149
Jaidip Patra <75015740+jaidippatra@users.noreply.github.com>
2020-11-28 09:16:12 +0530

    Create squareRoot.cpp

---
+++ 27-11-2020/squareRoot.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    double num, result;
+    cout<<"Enter any number: ";
+    cin>>num;
+    
+    result = sqrt(num);
+    cout<<"The square root of "<<num<<" is "<<result;
+    return 0;
+}
