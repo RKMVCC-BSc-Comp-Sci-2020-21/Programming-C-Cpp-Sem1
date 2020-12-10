491e92f41b7dc3bec28c327446bb11d420a880b5
Srijan Dutta <srijandutta200@gmail.com>
2020-12-11 00:04:52 +0530

    Create sum2.cpp

---
+++ December/09-12-2020/sum2.cpp
@@ -0,0 +1,19 @@
+//Find the sum of digits of a given number
+//Solution by Srijan Dutta
+
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cout<<"Enter an integer:\n";
+    cin>>n;
+    int s=0;
+    int i=n;
+    while(i!=0){
+        s+= (i%10);
+        i/=10;
+    }
+    cout<<"The sum of the digits of the entered number is:"<<s;
+}
