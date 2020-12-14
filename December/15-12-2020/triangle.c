1a4c664b841be767bcc536d4a7784f232dd40481
Jaidip Patra <75015740+jaidippatra@users.noreply.github.com>
2020-12-14 19:21:24 +0530

    Create triangle.c

---
+++ December/15-12-2020/triangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int s1,s2,s3;
+    printf("Enter three sides of triangle: ");
+    scanf("%d %d %d",&s1,&s2,&s3);
+    
+    if(s1==s2==s3)
+        printf("It is an equilateral triangle");
+    else if(s1==s2||s2==s3||s1==s3)
+        printf("It is an isosceles triangle");
+    else
+        printf("It is a scalene triangle");
+        
+    return 0;
+    
+}
