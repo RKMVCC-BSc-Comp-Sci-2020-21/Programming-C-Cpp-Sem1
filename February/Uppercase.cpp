0aef21830867286b5b152caa5a93b08161daa071
Srijan Dutta <srijandutta200@gmail.com>
2021-02-06 15:41:00 +0530

    Create Uppercase.cpp

---
+++ February/Uppercase.cpp
@@ -0,0 +1,18 @@
+#include <cctype>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    char str[] = "Welcome to rkmvcc";
+
+    cout << "The uppercase version of ' " << str  << " ' is: "<< endl;
+	cout << endl;
+    for (int i=0; i<strlen(str); i++)
+        putchar(toupper(str[i]));
+    cout << endl;
+    return 0;
+}
