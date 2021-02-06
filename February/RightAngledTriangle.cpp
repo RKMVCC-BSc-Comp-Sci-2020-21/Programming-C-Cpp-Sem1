d192727ca1a3ceec6ff20831c94de019583ec005
Srijan Dutta <srijandutta200@gmail.com>
2021-02-06 20:13:38 +0530

    Add files via upload

---
+++ February/RightAngledTriangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	int n;
+	cout << "Enter the number of rows: " << endl;
+	cin >> n;
+	
+	for(int i=1; i<=n; i++){
+		for(int j=1;j<=i; j++){
+				cout << "* " ;
+		}
+		cout << endl;
+	}
+}
