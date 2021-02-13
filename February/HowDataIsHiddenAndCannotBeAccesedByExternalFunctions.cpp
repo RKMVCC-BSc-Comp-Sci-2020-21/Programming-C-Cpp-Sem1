75c5ff373f3d9d5f8fa2642425e56b0728f1a040
Sayantan Mondal <75028962+sayankp@users.noreply.github.com>
2021-02-13 20:53:03 +0530

    Create HowDataIsHiddenAndCannotBeAccesedByExternalFunctions.cpp
    Basically shown via a simple program ,how data in CPP remains in the desired function.

---
+++ February/HowDataIsHiddenAndCannotBeAccesedByExternalFunctions.cpp
@@ -0,0 +1,19 @@
+//sayankp
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    {
+        int x,y;
+         x=10;             // removing brackets from 6th and 10th line makes the data avaiable for every fumctions.
+         y=23;
+    }  
+    
+       
+        int z= x+y;
+        cout<<z;
+
+   return 0;
+}
