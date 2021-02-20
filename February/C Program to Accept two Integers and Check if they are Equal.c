1eda32f835d3eafafbef807e0d0a50e78b9c0a38
ayan178 <79371494+ayan178@users.noreply.github.com>
2021-02-20 23:15:30 +0530

    Create C Program to Accept two Integers and Check if they are Equal.c

---
+++ February/C Program to Accept two Integers and Check if they are Equal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	
+	printf("Enter the value of a and b:  ");
+	scanf("%d%d",&a,&b);
+	
+	if(a==b)
+	  printf("M and N are equal");
+	  
+	else
+	  printf("M and N are not equal");
+	  
+  return 0;  
+}
