9c7a5aa56a1a13c6689292475e42bc8ec0d8ba5e
ayan178 <79371494+ayan178@users.noreply.github.com>
2021-02-24 10:42:59 +0530

    Create Enter number of month using switch case .c

---
+++ February/Enter number of month using switch case .c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+int main()
+{
+	int month;
+	printf("Enter the number of the month(1-12): ");
+	scanf("%d",&month);
+	
+	switch(month)
+	{
+		case 1:
+			printf("31 days");
+			break;
+		
+		case 2:
+			printf("28/29 days");
+			break;
+			
+		case 3:
+			printf("31 days");
+			break;
+			
+		case 4:
+			printf("30 days");
+			break;
+			
+		case 5:
+			printf("31 days");
+			break;
+			
+		case 6:
+			printf("30 days");
+			break;
+			
+		case 7:
+			printf("31 days");
+			break;	
+			
+		case 8:
+			printf("31 days");
+			break;	
+			
+		case 9:
+			printf("30 days");
+			break;
+			
+		case 10:
+			printf("31 days");
+			break;
+			
+		case 11:
+			printf("30 days");
+			break;
+			
+		case 12:
+			printf("31 days");
+			break;	
+				
+		default:
+			printf("Invalid month number.");		
+					
+					
+		}
+}
