93c094718030fcc92df3008968844b9d61e2ac4a
Joydipta-Biswas <79597886+Joydipta-Biswas@users.noreply.github.com>
2021-02-25 11:45:23 +0530

    Add files via upload

---
+++ February/GCD_&_LCM_CALCULATION_OF_2_GIVEN_NUMBERS.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a,b,x,y;
+
+    cout<<"Enter two numbers: ";
+
+    cin>>a>>b;
+
+    x=a;
+
+    y=b;
+
+    while(a!=b){
+
+        if(a>b){
+
+           a=a-b;
+
+        }else{
+
+           b=b-a;
+
+        }
+    }
+
+    cout<<"HCF/GCD of the given numbers is : "<<a<<endl;
+
+    cout<<"LCM of the given numbers is : "<<x*y/a<<endl;
+
+    return 0;
+}
