58d03dec24fee02e12bc73e247b363960a9f8ba1
Saumyadip1 <75024072+Saumyadip1@users.noreply.github.com>
2021-02-25 21:20:51 +0530

    Add files via upload

---
+++ February/neg_pos_Saumyadip_Kar.c
@@ -0,0 +1,13 @@
+    
+    //By Saumyadip Kar
+	#include <stdio.h>
+    void main()
+    {
+        int num;
+        printf("Enter a number\n");
+        scanf("%d", &num);
+        if (num >= 0)
+            printf("%d is positive\n", num);
+        else
+            printf("%d is negative\n", num);
+    }
