066cea6766e1a069c5fbb8977a9b70f130dbafce
Joydipta-Biswas <79597886+Joydipta-Biswas@users.noreply.github.com>
2021-02-25 11:56:35 +0530

    Add files via upload

---
+++ February/SUM_OF_FIRST_10_NATURAL_NUMBERS.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int total = 0;
+
+    int n = 10;
+
+    for(int i = 1 ; i<=n ; i++ ){
+
+        total=total+i;
+
+    }
+
+    cout<<"Sum of first "<<n<<" natural number : "<<total<<endl;
+
+    return 0;
+}
