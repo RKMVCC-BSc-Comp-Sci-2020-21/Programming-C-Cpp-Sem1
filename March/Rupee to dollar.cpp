6fa93ee6f0a8450e80e30034ea077b27f40bd2cf
SOUVIK MONDAL <78786416+SOUVIK894@users.noreply.github.com>
2021-03-07 21:27:15 +0530

    Create Rupee to dollar.cpp

---
+++ March/Rupee to dollar.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int rs=10;
+	float dol=0;
+	cout<<"Rupees Dollar"<<endl;
+	
+	cout<<"__________________"<<endl;
+	for(rs=10;rs<=250;rs+=5)
+	{
+		dol=(float)rs/73.66;
+		cout<<"Rs."<<rs<<"="<<"$"<<dol<<endl;
+	}
+	return 0;
+}
