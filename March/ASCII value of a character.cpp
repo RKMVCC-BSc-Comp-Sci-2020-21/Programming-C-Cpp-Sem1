3b511f61cadbaecbf60d4a394ffec98767d67772
ArkodipGanguly7 <80024038+ArkodipGanguly7@users.noreply.github.com>
2021-03-07 11:48:15 +0530

    Create ASCII value of a character.cpp

---
+++ March/ASCII value of a character.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char asciichar;
+    cout << "Enter a character : ";
+    cin >> asciichar;
+    cout << "\nIt's ascii  value is : " << (int) asciichar << endl;
+    return 0;
+}
