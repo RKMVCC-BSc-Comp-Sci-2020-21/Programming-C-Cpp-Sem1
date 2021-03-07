cabc5b5a3a4462eef0c28b227f53378a9bdd56b3
ArkodipGanguly7 <80024038+ArkodipGanguly7@users.noreply.github.com>
2021-03-07 12:24:02 +0530

    Create Area of square.cpp

---
+++ March/Area of square.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+  {
+    int area,side;
+    cout<<"Enter side of square\n";
+    cin>>side;
+    area=side*side;
+    cout<<"Area of square="<<area;
+  }
