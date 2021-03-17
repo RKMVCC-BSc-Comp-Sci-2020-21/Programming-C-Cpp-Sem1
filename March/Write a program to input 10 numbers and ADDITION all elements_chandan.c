db6eb641cf43a336b9be5102dd8987c8c98ee4f3
CHANDAN ADHIKARY <75032996+CHANDAN-08@users.noreply.github.com>
2021-03-17 02:28:56 -0500

    Create  Write a program to input 10 numbers and ADDITION all elements_chandan.c

---
+++ March/Write a program to input 10 numbers and ADDITION all elements_chandan.c
@@ -0,0 +1,18 @@
+// Write a program to input 10 numbers and ADDITION all elements.
+
+#include<stdio.h>
+
+main()
+
+{
+   int i, values[10];
+   printf("Enter 10 integers:\n");
+   for(i=1; i<10;++i)
+   {
+      scanf("%d",&values[i]);
+       sum += values[i];
+    
+  }
+  printf("sum of elements of Array=%d\n");
+  
+}  
