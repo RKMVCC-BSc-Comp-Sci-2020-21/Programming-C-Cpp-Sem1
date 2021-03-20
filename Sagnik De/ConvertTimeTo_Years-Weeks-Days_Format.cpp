6cc2ac005c5710b277dc6c2e917c426b50e52589
Sagnik De <75027846+Sagnik-De@users.noreply.github.com>
2021-03-20 20:37:33 +0530

    Create ConvertTimeTo_Years-Weeks-Days_Format.cpp

---
+++ Sagnik De/ConvertTimeTo_Years-Weeks-Days_Format.cpp
@@ -0,0 +1,21 @@
+//WAP to convert days into Years:Weeks:Days format
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int val, yrs, wks, day, rem;
+
+    cout<<"Enter the value in days --> ";
+    cin>>val;
+
+    yrs = val/365;
+    rem = val%365;
+    wks = rem/7;
+    day = rem%7;
+
+    cout<<"\n\t"<<val<<" days is equivalent to "<<yrs<<" years, "<<wks<<" weeks and "<<day<<" days.\n\n";
+
+    return 0;
+}
