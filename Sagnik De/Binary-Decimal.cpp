aa05e82709edc8d15fb4cadda96178f83c0719ba
Sagnik De <75027846+Sagnik-De@users.noreply.github.com>
2021-03-20 20:33:41 +0530

    Create Binary-Decimal.cpp

---
+++ Sagnik De/Binary-Decimal.cpp
@@ -0,0 +1,33 @@
+//WAP to convert binary to decimal no.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int ConvertToDecimal(int bin)
+{
+    int r, t, dig=0, eqv=0;
+
+    while(bin>0)
+    {
+        r = bin%10;
+        t = (pow(2,dig++)) * r;
+        eqv += t;
+        bin /= 10;
+    }
+    return eqv;
+}
+
+int main()
+{
+    int bin, dec;
+
+    cout<<"Enter the binary number --> ";
+    cin>>bin;
+
+    dec = ConvertToDecimal(bin);
+
+    cout<<"\n\tResult ---> "<<dec<<"\n\n";
+
+    return 0;
+}
