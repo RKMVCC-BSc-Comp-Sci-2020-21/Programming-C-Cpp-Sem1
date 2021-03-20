a13d728abc0da23a12229713924cfa40a4a9bed1
Suvadip-sana <78638404+Suvadip-sana@users.noreply.github.com>
2021-03-20 18:39:36 +0530

    Create takeTwoNumberAndAddThem.c

---
+++ March/takeTwoNumberAndAddThem.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int x,y,z;
+	printf("enter first number:");
+	scanf("%d",&x);
+	printf("enter second number:");
+	scanf("%d",&y);
+	z=x+y;
+	printf("the result is: %d",z);
+	
+	return 0;
+	
+}
