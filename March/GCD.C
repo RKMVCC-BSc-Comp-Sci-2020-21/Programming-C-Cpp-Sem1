25c61b5398b92c0eb5b2ee8be9022bc3352eb776
Sumitde-art <81013488+Sumitde-art@users.noreply.github.com>
2021-03-20 21:14:23 +0530

    Create GCD.C

---
+++ March/GCD.C
@@ -0,0 +1,19 @@
+// GCD //
+
+#include<stdio.h>
+
+int main()
+{
+	int a,b,gcd;
+	printf("Enter two numbers: ");
+	scanf("%d %d",&a,&b);
+	for(int i=1;i<=a&&i<=b;i++)
+	{
+		if(a%i==0 && b%i==0)
+		{
+			gcd=i;
+		}
+	 } 
+	 printf("GCD of two numbers is %d",gcd);
+	 return 0;
+}
