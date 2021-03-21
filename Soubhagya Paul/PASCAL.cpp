3945f159a68d8aa54abfc77f044a4d386d6ebd8d
SOubhagyaPaul <77911463+SOubhagyaPaul@users.noreply.github.com>
2021-03-22 00:11:37 +0530

    Add files via upload

---
+++ Soubhagya Paul/PASCAL.cpp
@@ -0,0 +1,55 @@
+//PASCAL'S TRIANGLE
+#include <conio.h>
+#include <iostream.h>
+
+void main()
+{
+ clrscr();
+ int a[100];
+ int n,i,j;
+
+ cout<<"NUMBER OF LINE(S) ? (SHOULD BE LESS THAN 12): ";
+
+ //ENTERING THE NUMBER OF LINES:
+
+ do
+ {
+  cin>>n;
+  if(n<1 || n>11)
+	{cout<<endl;
+	 cout<<"ERROR!!ENTER AGAIN!";
+	}
+ }while (n<1 || n>11);
+
+ cout<<endl;
+
+ //GENERATING THE TRIANGLE BY A NESTED LOOP
+
+ a[0]=1;
+
+ for(i=0;i<n;i++)
+  { cout<<endl;
+
+	 for(j=0;j<32-(i*3);j++)
+		 cout<<" ";
+
+	 for(j=0;j<=i;j++)
+		{cout<<a[j]<<"   ";
+
+		 if(a[j]<100)
+			cout<<" ";
+
+		 if(a[j]<10)
+			cout<<" ";
+		}
+
+	 for(j=i+1;j>0;j--)
+		{
+		 if(j==0 || j==i+1)
+			a[j]=1;
+		 else
+			a[j]=a[j-1]+a[j];
+		}
+  }
+ getch();
+}
