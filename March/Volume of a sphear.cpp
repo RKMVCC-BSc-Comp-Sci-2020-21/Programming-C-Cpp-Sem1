93b4d34dcb706ffccf65fc9389c4f195ceb2be62
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 09:51:48 +0530

    Create Volume of a sphear.cpp

---
+++ March/Volume of a sphear.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+int red1;
+float vol;
+cout<<"Calculate the volume of sphear : \n";
+cout<<"............................\n";
+cout<<"Enter the radious of the sphear : \n";
+cin>>vol;
+
+vol=(4*3.14*red1*red1*red1)/3;
+cout<<"The volume of the sphear is : "<<vol<<endl;
+cout<<endl;
+return 0;
+
