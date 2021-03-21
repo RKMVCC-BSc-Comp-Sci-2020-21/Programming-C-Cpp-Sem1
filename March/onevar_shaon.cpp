6bf652120867187bc11fbe956be0e90e5d952017
Shaon18 <75033737+Shaon18@users.noreply.github.com>
2021-03-21 11:46:42 +0530

    Create onevar_shaon.cpp

---
+++ March/onevar_shaon.cpp
@@ -0,0 +1,85 @@
+// C++ program to check if a string can be made
+// valid by removing at most 1 character.
+#include<bits/stdc++.h>
+using namespace std;
+
+// Assuming only lower case characters
+const int CHARS = 26;
+
+/* To check a string S can be converted to a “valid”
+string by removing less than or equal to one
+character. */
+bool isValidString(string str)
+{
+	int freq[CHARS] = {0};
+
+	// freq[] : stores the frequency of each
+	// character of a string
+	for (int i=0; i<str.length(); i++)
+		freq[str[i]-'a']++;
+
+	// Find first character with non-zero frequency
+	int i, freq1 = 0, count_freq1 = 0;
+	for (i=0; i<CHARS; i++)
+	{
+		if (freq[i] != 0)
+		{
+			freq1 = freq[i];
+			count_freq1 = 1;
+			break;
+		}
+	}
+
+	// Find a character with frequency different
+	// from freq1.
+	int j, freq2 = 0, count_freq2 = 0;
+	for (j=i+1; j<CHARS; j++)
+	{
+		if (freq[j] != 0)
+		{
+			if (freq[j] == freq1)
+			count_freq1++;
+			else
+			{
+				count_freq2 = 1;
+				freq2 = freq[j];
+				break;
+			}
+		}
+	}
+
+	// If we find a third non-zero frequency
+	// or count of both frequencies become more
+	// than 1, then return false
+	for (int k=j+1; k<CHARS; k++)
+	{
+		if (freq[k] != 0)
+		{
+			if (freq[k] == freq1)
+			count_freq1++;
+			if (freq[k] == freq2)
+			count_freq2++;
+			else // If we find a third non-zero freq
+			return false;
+		}
+
+		// If counts of both frequencies is more than 1
+		if (count_freq1 > 1 && count_freq2 > 1)
+		return false;
+	}
+
+	// Return true if we reach here
+	return true;
+}
+
+// Driver code
+int main()
+{
+	char str[] = "abcbc";
+
+	if (isValidString(str))
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+	return 0;
+}
