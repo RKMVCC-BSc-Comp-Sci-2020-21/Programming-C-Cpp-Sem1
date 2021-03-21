ae5fa66c67169dcb5da6e8b751f16765047f0ba8
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 10:24:57 +0530

    Create Polynomial.c

---
+++ March/Polynomial.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main()
+{
+    int a,b,c,d;
+    int p1,p2,p3;
+    printf("%d",INT_MAX);
+    c=INT_MAX+1;
+    printf("\n %d",c);
+
+    /*printf("Enter the value of a: ");
+    scanf("%d",&a);
+    printf("\nEnter the value of b: ");
+    scanf("%d",&b);
+    printf("\nEnter the value of c: ");
+    scanf("%d",&c);
+    printf("\nEnter the value of d: ");
+    scanf("%d",&d);
+    
+    p1=a*c;
+    p2=b*c + a*d;
+    p3=b*d;
+    if(p2<0 && p3<0)
+    {
+           printf("\n The resulting polynomial is %dx^2 %dx %d",p1,p2,p3);
+    }
+    else if(p2<0)
+    {
+        printf("\n The resulting polynomial is %dx^2 %dx +%d",p1,p2,p3);
+    }
+    else if(p3<0)
+    {
+        printf("\n The resulting polynomial is %dx^2 +%dx %d",p1,p2,p3);
+    }
+    else
+    {
+        printf("\n The resulting polynomial is %dx^2 +%dx +%d",p1,p2,p3);
+    }*/
+    
+
+    return 0;
+
+
+
+}
