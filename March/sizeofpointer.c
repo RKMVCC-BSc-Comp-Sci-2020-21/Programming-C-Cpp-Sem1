36d235d086a2d675e46fbd7a65ac04e4bc484de5
Sumit834 <78863130+Sumit834@users.noreply.github.com>
2021-03-21 20:51:55 +0530

    Create sizeofpointer.c

---
+++ March/sizeofpointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main()
+{
+    printf("\nsize of char pointer: %d"     ,sizeof(char*));
+    printf("\nsize of int pointer: %d"      ,sizeof(int*));
+    printf("\nsize of float pointer: %d"    ,sizeof(float*));
+    printf("\nsize of long int pointer: %d" ,sizeof(long int*));
+    printf("\nsize of double pointer: %d\n" ,sizeof(double*));
+    return 0;
+}
