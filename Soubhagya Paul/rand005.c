77ac2d46169c1377521a84a1a95bc86f78158e4f
SOubhagyaPaul <77911463+SOubhagyaPaul@users.noreply.github.com>
2021-03-21 19:08:55 +0530

    Create rand005.c

---
+++ Soubhagya Paul/rand005.c
@@ -0,0 +1,16 @@
+//   accept two integers and check whether they are equal or //not.
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("\n\tEnter a number : ");
+	scanf("%d",&a);
+	printf("\n\tEnter a number : ");
+	scanf("%d",&b);
+	if(a==b)
+	printf("\n\tThe numbers are equal . ");
+	else
+	printf("\n\tThe numbers are not equal . ");
+	printf(“\n\tBy Soubhagya paul”);
+	return 0;
+}
