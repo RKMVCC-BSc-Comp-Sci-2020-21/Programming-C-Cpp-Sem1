173bec71bb2ae7cd2fa1080835780658a856128a
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 10:14:18 +0530

    Create To find profit  or loss.c

---
+++ March/To find profit  or loss.c
@@ -0,0 +1,27 @@
+//To find profit  or loss
+
+#include <stdio.h>
+
+int main()
+{
+    float bp,sp,loss,profit;
+
+    printf("Enter buying price: ");
+    scanf("%f",&bp);
+
+    printf("Enter selling price: ");
+    scanf("%f",&sp);
+
+    if(sp>=bp)
+    {
+        profit= ((sp-bp)/bp)*100;
+        printf("Your profit is %f%%",profit);
+    }
+    else
+    {
+        loss = ((bp-sp)/bp)*100;
+        printf("Your loss is %f%%",loss);
+    }
+
+    return 0;
+}
