e28afbd94914faea13bce5fc3653f6d2f3340234
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 10:08:04 +0530

    Create Digit to month.c

---
+++ March/Digit to month.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("Enter any number 0 to 9 :");
+	scanf("%d", &a);
+	switch(a)
+	{
+		
+		case 1:
+			printf("\n January");
+			break;
+			
+		case 2:
+			printf("\n Febrary");
+			break;
+			
+		case 3:
+			printf("\n March");
+			break;
+			
+		case 4:
+			printf("\n April");
+			break;
+			
+			
+		case 5:
+			printf("\n May");
+			break;
+			
+		case 6:
+			printf("\n June");
+			break;
+			
+		case 7:
+			printf("\n July");
+			break;	
+			
+		case 8:
+			printf("\n August");
+			break;
+			
+		case 9:
+			printf("\n September");
+			break;		
+									
+	}
+}
