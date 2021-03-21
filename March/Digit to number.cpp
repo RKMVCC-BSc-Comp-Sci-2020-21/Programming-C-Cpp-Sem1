d558740ee02d9739dbe887f6bc0c92c09e366d2e
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 09:11:46 +0530

    Create Digit to number.cpp

---
+++ March/Digit to number.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<<"Enter any number 0 to 9 :";
+	cin>>a;
+	switch(a)
+	{
+		case 0:
+			cout<<"\n zero";
+			break;
+		
+		case 1:
+			cout<<"\n one";
+			break;
+			
+		case 2:
+			cout<<"\n two";
+			break;
+			
+		case 3:
+			cout<<"\n three";
+			break;
+			
+		case 4:
+			cout<<"\n four";
+			break;
+			
+			
+		case 5:
+			cout<<"\n five";
+			break;
+			
+		case 6:
+			cout<<"\n six";
+			break;
+			
+		case 7:
+			cout<<"\n seven";
+			break;	
+			
+		case 8:
+			cout<<"\n eight";
+			break;
+			
+		case 9:
+			cout<<"\n nine";
+			break;		
+		
+		return 0;							
+	}
+}
