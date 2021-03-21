1a6be59141d1bff8fb986ec8a60ae8c533269c27
Sayantan Mondal <75028962+sayankp@users.noreply.github.com>
2021-03-21 23:04:41 +0530

    Add files via upload

---
+++ sayantan mondal/Area of square.cpp
@@ -0,0 +1,12 @@
+// Area of square.cpp //
+
+#include<iostream>
+using namespace std;
+int main()
+  {
+    int area,side;
+    cout<<"Enter side of square\n";
+    cin>>side;
+    area=side*side;
+    cout<<"Area of square="<<area;
+  }
