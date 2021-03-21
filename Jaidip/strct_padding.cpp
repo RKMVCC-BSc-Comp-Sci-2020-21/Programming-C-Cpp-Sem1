45e2ac0f89d581791f01ea2986b28e5c92124814
Jaidip Patra <75015740+jaidippatra@users.noreply.github.com>
2021-03-21 09:17:32 +0530

    Add files via upload

---
+++ Jaidip/strct_padding.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    struct abc
+    {
+       char a;
+       char b;
+       int abc;
+       int d;
+
+    }v1;
+
+    cout<<sizeof(v1);
+    
+}
