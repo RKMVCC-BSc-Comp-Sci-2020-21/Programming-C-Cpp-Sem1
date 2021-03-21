260741c0f01dba3b3c94ff7d3b6791925b7d4e3a
Sudipta Sarkar <75057281+Sudipta360sarkar@users.noreply.github.com>
2021-03-21 13:41:28 +0530

    Create check if input Number is int or float by sudipta sarkar.c

---
+++ March/check if input Number is int or float by sudipta sarkar.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+   
+    char number[10];
+    int flag = 0;
+    int length, i = 0;
+
+    printf("\n\nEnter a number: ");
+    scanf("%s", number);
+
+    length = strlen(number);
+
+  
+    while(number[i++] != '\0')   
+    {
+        if(number[i] == '.')    
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+  
+    if(flag)
+        printf("\n\n\n\tEntered Number is a Floating point Number\n\n");
+    else
+        printf("\n\n\n\tEntered Number is a integer Number\n\n");
+
+   
+    return 0;
+}
