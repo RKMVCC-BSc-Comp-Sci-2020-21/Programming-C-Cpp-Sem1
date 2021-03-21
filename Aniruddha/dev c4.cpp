557fac6a795167a1c3c652929203496d1e0165d5
Aniruddha-karmokar <75034331+Aniruddha-karmokar@users.noreply.github.com>
2021-03-22 00:34:16 +0530

    Add files via upload

---
+++ Aniruddha/dev c4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+	int a,b,c;
+	cout<<"enter the first number:  ";
+	cin>>"%d",&a;
+	cout<<"enter the second number:  ";
+	cin>>"%d",&b;
+	c=a+b;
+	cout<<"result is %d",c;
+	return 0;
+
+}
