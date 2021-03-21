09f2c627b7e39ee0be669a2bb1092ab85dd424b1
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 09:35:51 +0530

    Create Multipication table of any number.cpp

---
+++ March/Multipication table of any number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,k;
+	int n;
+	
+	cout<<"Put the value of n :";
+	cin>>n;
+	for(i=1;i<=20;i++)
+	{
+		cout<<"\n nx"<<i<<"="<<n*i;
+	}
+	return 0;
+	
+	
+}
