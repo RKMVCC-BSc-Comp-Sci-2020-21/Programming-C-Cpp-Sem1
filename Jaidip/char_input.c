8be782362996577a3c8cf605505f7d2a408757de
Jaidip Patra <75015740+jaidippatra@users.noreply.github.com>
2021-03-21 09:05:46 +0530

    Delete Jaidip directory

--- Jaidip/char_input.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-#include<conio.h>
-// Example for getche() in C 
-int main() 
-{ 
-printf("%c", getche()); 
-return 0; 
-}
--- Jaidip/type_conversion.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-//#include<math.h>
-int main()
-{
-    int sum = 467;
-    double root = sqrt(sum);
-    printf("The average is %lf",root);
-    return 0;
-}
