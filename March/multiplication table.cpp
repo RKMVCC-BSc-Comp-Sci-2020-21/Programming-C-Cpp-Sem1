3eeebea6c7ad80bbac3fabeba9f49aae744c4410
Sudipta Sarkar <75057281+Sudipta360sarkar@users.noreply.github.com>
2021-03-21 13:52:06 +0530

    Create multiplication table.cpp

---
+++ March/multiplication table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=2, i, res;
+    for(i=1; i<=10; i++)
+    {
+        res = num*i;
+        cout<<num<<" * "<<i<<" = "<<res;
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
