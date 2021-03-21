22831ea900319a5a74adeb454437dc80b255d720
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 09:49:41 +0530

    Create TWO NUMBERS ARE EQUAL OR NOT .cpp

---
+++ March/TWO NUMBERS ARE EQUAL OR NOT .cpp
@@ -0,0 +1,18 @@
+// TWO NUMBERS ARE EQUAL OR NOT 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int m,n;
+	cout<<"Enter the values of M and N\n";
+	cin>>m>>n ;
+	
+	if (m == n)
+		cout<<"M and N are equal\n ";
+	
+	else 
+		cout<<"M and N are not equal\n ";
+		
+	return 0;
+}
