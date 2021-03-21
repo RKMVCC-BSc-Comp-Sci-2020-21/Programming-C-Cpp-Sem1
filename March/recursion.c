c518120f04839ffaa530a01330396d75594bb166
Souptik09 <75028313+Souptik09@users.noreply.github.com>
2021-03-21 22:22:57 +0530

    Create recursion.c

---
+++ March/recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+   int num,rem,reverse_num=0;
+   //Input number
+   printf("\nEnter any number:");
+   scanf("%d",&num);
+
+   while(num>=1)
+   {
+      rem = num % 10;
+      reverse_num = reverse_num * 10 + rem;
+      num = num / 10;
+   }
+
+   printf("\nReverse of input number is: %d", reverse_num);
+   return 0;
+}
