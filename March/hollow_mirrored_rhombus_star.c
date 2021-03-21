63395ac8937ec53111b638a72d635ed7c07da608
Avirupdutta-2001 <75062211+Avirupdutta-2001@users.noreply.github.com>
2021-03-21 23:31:50 +0530

    Create hollow_mirrored_rhombus_star.c

---
+++ March/hollow_mirrored_rhombus_star.c
@@ -0,0 +1,27 @@
+// Name:- Avirup Dutta, Roll No:- 721
+
+// The code for the hollow mirrored rhombus star pattern is given below:
+
+#include <stdio.h>  
+int main()  
+{  
+    int n;  
+    printf("Enter the number of rows");  
+    scanf("%d",&n);  
+    for(int i=1;i<=n;i++)  
+    {  
+        for(int j=1;j<i;j++)  
+        {  
+            printf(" ");  
+        }  
+        for(int k=1;k<=n;k++)  
+        {  
+         if(i==1 || i==n || k==1 || k==n)  
+           printf("*");  
+            else  
+            printf(" ");  
+        }  
+        printf("\n");  
+    }  
+    return 0;  
+}  
