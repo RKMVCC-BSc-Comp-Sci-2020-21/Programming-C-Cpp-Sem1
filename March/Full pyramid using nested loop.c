f5f8a5cf6c6a3db4de744393981e323eb3782b57
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 10:37:46 +0530

    Create Full pyramid using nested loop.c

---
+++ March/Full pyramid using nested loop.c
@@ -0,0 +1,34 @@
+/* Diagram print (5) using NESTED LOOP/ Full Pyramid of *.
+                       *
+                      ***
+                     *****
+                    *******   */
+                    
+                    
+
+#include<stdio.h>
+
+main()
+
+
+{
+
+    int i,j,k,n;
+    printf("Enter the number of rows =\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)                        
+  {
+          for(j=1;j<=n-i;j++)
+          {
+            printf(" ");
+           }
+              for(k=1;k<=((2*i)-1);k++)
+             {
+               printf("*");
+             }
+          
+         printf("\n");
+         }
+         
+         
+} 
