ea42b7ba8eb0773d48a310563587e9b3cdb6e491
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 09:46:07 +0530

    Create Size of variables.c

---
+++ March/Size of variables.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+	
+	//SIZE OF ALL VARIABLES
+	
+	printf("\nSIZE OF INT:%d ", sizeof(intType));
+	printf("\nSIZE OF FLOAT: %d ", sizeof(floatType));
+	printf("\nSIZE OF DOUBLE:%d ", sizeof(doubleType));
+	printf("\nSIZE OF CHAR: %d ", sizeof(charType));
+	
+	
+	return 0;
+}
