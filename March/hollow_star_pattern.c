c1cc7033765de344de8c226b63bc41e43143646f
Avirupdutta-2001 <75062211+Avirupdutta-2001@users.noreply.github.com>
2021-03-21 21:49:25 +0530

    Create hollow_star_pattern.c

---
+++ March/hollow_star_pattern.c
@@ -0,0 +1,26 @@
+// Name:- Avirup Dutta  ;  Roll No:- 721
+
+// The code for the hollow square star pattern is given below:
+
+#include <stdio.h>
+int main()  
+{  
+    int n;  
+    printf("Enter the number of rows");  
+    scanf("%d",&n);  
+    for(int i=1;i<=n;i++)  
+    {  
+        for(int j=1;j<=n;j++)  
+        {  
+            if(i==1 ||i==n||j==1||j==n)  
+            {  
+            printf("*");  
+            }  
+            else  
+            printf(" ");  
+        }  
+        printf("\n");  
+    }  
+      
+    return 0;  
+}  
