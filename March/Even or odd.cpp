017bbbe9590af0ea0fd287a47762ac1dd34dd42b
SOUVIK MONDAL <78786416+SOUVIK894@users.noreply.github.com>
2021-03-21 21:33:27 +0530

    Create Even or odd.cpp

---
+++ March/Even or odd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  int no;
+  cout << "Enter any number: ";
+  cin >> no;
+  if(no%2==0)
+  {
+    cout<<"Even number";
+  }
+  else
+  {
+    cout<<"Odd number";
+  }
+  return 0;
+}
