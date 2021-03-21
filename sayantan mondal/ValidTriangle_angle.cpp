fef23ae67620e78cc092ff12e653eff5c8b755d2
Sayantan Mondal <75028962+sayankp@users.noreply.github.com>
2021-03-21 19:32:07 +0530

    Add files via upload

---
+++ sayantan mondal/ValidTriangle_angle.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a;
+ int b;
+ int c;
+ cout<<"enter three angle of a triangle"<<endl;
+ cin>>a>>b>>c;
+ int sum=a+b+c;
+ if(sum==180 && a!=0 && b!=0 && c!=0)
+ {
+ cout<<"the triangle is valid"<<endl;
+}
+else
+{
+cout<<"the triangle is not valid"<<endl;
+}
+ 
+ 
+}
