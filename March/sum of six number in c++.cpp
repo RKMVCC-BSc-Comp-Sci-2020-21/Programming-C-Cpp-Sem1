fe77f8c94b9dac09d39aed6b123584de687b1cbb
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 09:44:55 +0530

    Create sum of six number in c++.cpp

---
+++ March/sum of six number in c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{ 
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+cout <<"Enter six integers :";
+cin >>a>>b>>c>>d>>e>>f;
+cout <<a+b+c+d+e+f;
+return 0;
+}
