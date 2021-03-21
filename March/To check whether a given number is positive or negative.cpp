dd5e2c7c54aea415627f4f5b77edddfe3136fc0d
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 08:57:34 +0530

    Create To check whether a given number is positive or negative.cpp

---
+++ March/To check whether a given number is positive or negative.cpp
@@ -0,0 +1,17 @@
+// to check whether a given number is positive or negative.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<<"Enter a number ";
+	cin>>a;
+	
+	if (a>=0)
+		cout<<"a is positive\n";
+	else 
+		cout<<"a is negative\n";
+	
+	return 0;	
+}
