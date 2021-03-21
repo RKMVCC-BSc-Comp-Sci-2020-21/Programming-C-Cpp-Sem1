edb811d92b862f1d6df4df40d886edf0e5050c29
suhindubey <79895355+suhindubey@users.noreply.github.com>
2021-03-21 08:47:14 +0530

    Create Binary to Hexa.cpp

---
+++ March/Binary to Hexa.cpp
@@ -0,0 +1,46 @@
+ //Binary to Hexadecimal in C++
+ # include <iostream>
+ #include<math.h>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int num ,rem,des=0,i=0,j=0,temp;
+ 	char hexa[100];
+ 	cout<<"Enter any binary number ";
+ 	cin>>num;
+ 	temp=num;
+ 	while (temp>0)
+ 	{
+ 		rem=temp%10;
+ 		des=des+rem*pow(2,i);
+ 		i++;
+ 		temp=temp/10;
+ 		
+	 }
+	 i=0;
+	 rem=0;
+	 while(des>0)
+	 {
+	 	rem=des%16;
+	 	if(rem<10)
+	 	{
+	 		hexa[i]=rem+40;
+	 		
+	     }
+	     else
+		{
+		 	hexa[i]=rem+55;
+		 	
+		}
+		 i++;
+		 des=des/16;
+		 
+	 }
+	 for (j=i-1;j>=0;j--)
+	 {
+	 	cout<<hexa[j];
+	 }
+	 return 0;
+ 
+ }
