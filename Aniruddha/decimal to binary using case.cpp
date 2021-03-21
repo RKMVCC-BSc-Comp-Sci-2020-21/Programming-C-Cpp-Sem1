e41561c34a6f4c31406ac723d2fa6c7e98547fee
Aniruddha-karmokar <75034331+Aniruddha-karmokar@users.noreply.github.com>
2021-03-22 00:25:26 +0530

    Add files via upload

---
+++ Aniruddha/decimal to binary using case.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class dectobin
+{
+	public:
+		 int binaryNum[32]; 
+  
+     int i = 0; 
+    while (int n > 0)
+	 { 
+  
+     
+        binaryNum[i] = n % 2; 
+        n = n / 2; 
+        i++; 
+    }   
+    for (int j = i - 1; j >= 0; j--) 
+        cout << binaryNum[j]; 	
+		}
+		 
+};
+int main()
+{
+ dectobin obj;
+ obj.binaryNum;
+ return 0;
+ 	
+}
