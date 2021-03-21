0feffeabb0f06efdc51d617d6618d2aa459b7efb
ARIT SAMADDAR <75027931+Arit955@users.noreply.github.com>
2021-03-21 19:46:40 +0530

    Create operator.c

---
+++ March/operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+	int a, b;
+	float s;
+	printf ("\n enter the value for a:");
+	scanf ("%d", &a);
+	printf ("\n enter the value for b:");
+	scanf ("%d", &b);
+	s=(float) a/b;
+	printf ("\n the result is=%f", s);
+	return (0); 
+	}
