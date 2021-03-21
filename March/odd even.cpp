1cef9ed2c67e5cbf61a25d4031ada9839574eec6
Sourav Halder <75027860+souravh10@users.noreply.github.com>
2021-03-21 10:36:39 +0530

    Create odd even.cpp

---
+++ March/odd even.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+ {
+ 	 int x;
+ 	 printf("enter a number :\n");
+ 	 scanf("%d",&x);
+ 	 if (x%2==0)
+ 	    printf ("the given number is even");
+ 	 else 
+	    printf("the given number is odd");  
+return 0;		  
+ }
