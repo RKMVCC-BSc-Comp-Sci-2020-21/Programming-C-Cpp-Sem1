f2db2febce940644ecaef7da945e249111adad93
Avirupdutta-2001 <75062211+Avirupdutta-2001@users.noreply.github.com>
2021-03-22 07:16:45 +0530

    Create hollow_inverted_right_triangle_star_pattern.c

---
+++ March/hollow_inverted_right_triangle_star_pattern.c
@@ -0,0 +1,24 @@
+// Name:- Avirup Dutta  ;  Roll No:- 721
+
+// The code for the hollow inverted right triangle star pattern is given below:
+
+#include <stdio.h>  
+  
+int main()  
+{  
+    int n,m=1;  
+    printf("Enter the number of rows");  
+    scanf("%d",&n);  
+    for(int i=n;i>=1;i--)  
+    {  
+      for(int j=1;j<=i;j++)  
+      {  
+         if(j==1 || j==i || i==n)  
+          printf("*");  
+          else  
+          printf(" ");  
+      }  
+      printf("\n");  
+    }  
+    return 0;  
+}  
