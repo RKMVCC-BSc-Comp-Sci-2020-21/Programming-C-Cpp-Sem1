cc8a7ddf5287d163ef4033858da2f4c398545df5
Avirupdutta-2001 <75062211+Avirupdutta-2001@users.noreply.github.com>
2021-03-22 07:35:16 +0530

    Create Plus_Star_Pattern.c

---
+++ March/Plus_Star_Pattern.c
@@ -0,0 +1,32 @@
+// Name:- Avirup Dutta  ;  Roll No:- 721
+  
+// The code for the plus star pattern is given below:
+
+#include <stdio.h>  
+  
+int main(void) {  
+  int n;  
+  printf("Enter the odd number only");  
+  scanf("%d", &n);  
+  for(int i=1;i<=n;i++)  
+  {  
+    if(i==((n/2)+1))  
+    {  
+      for(int j=1;j<=n;j++)  
+      {  
+        printf("+");  
+      }  
+   
+    }  
+    else  
+    {  
+    for(int j=1;j<=n/2;j++)  
+    {  
+      printf(" ");  
+    }  
+    printf("+");  
+    }  
+    printf("\n");  
+  }  
+  return 0;  
+}  
