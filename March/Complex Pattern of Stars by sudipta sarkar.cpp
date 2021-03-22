07eb3d49e60f5497921fd86afa6460f9131c630a
Sudipta Sarkar <75057281+Sudipta360sarkar@users.noreply.github.com>
2021-03-22 08:59:01 +0530

    Create Complex Pattern of Stars by sudipta sarkar.cpp

---
+++ March/Complex Pattern of Stars by sudipta sarkar.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    for(i=0; i<6; i++)
+    {
+        for(j=0; j<=i; j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
