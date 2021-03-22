e1bbef21f60cc168550a152d55ad79dfba75afb9
Avirupdutta-2001 <75062211+Avirupdutta-2001@users.noreply.github.com>
2021-03-22 07:42:47 +0530

    Create if_statement.c

---
+++ March/if_statement.c
@@ -0,0 +1,12 @@
+// Name:- Avirup Dutta  ;  Roll No:- 721
+
+#include<stdio.h>    
+int main(){    
+int number=0;    
+printf("Enter a number:");    
+scanf("%d",&number);    
+if(number%2==0){    
+printf("%d is even number",number);    
+}    
+return 0;  
+}    
