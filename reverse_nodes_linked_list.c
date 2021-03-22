08d36cfa60f710614826c3d4e299d11966588ffe
Saumyadip1 <75024072+Saumyadip1@users.noreply.github.com>
2021-03-22 09:53:35 +0530

    Add files via upload

---
+++ reverse_nodes_linked_list.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+
+};
+
+
+void print_reverse_recursive (struct node *);
+
+void print (struct node *);
+
+void create_new_node (struct node *, int );
+
+
+int main ()
+
+{
+
+    struct node *head = NULL;
+
+    insert_new_node (&head, 1);
+
+    insert_new_node (&head, 2);
+
+    insert_new_node (&head, 3);
+
+    insert_new_node (&head, 4);
+
+    printf ("LinkedList : ");
+
+    print (head);
+
+    printf ("\nLinkedList in reverse order : ");
+
+    print_reverse_recursive (head);
+
+    printf ("\n");
+
+    return 0;
+
+}
+
+
+
+void print_reverse_recursive (struct node *head)
+
+{
+
+    if (head == NULL)
+
+    {
+
+        return;
+
+    }
+
+
+    print_reverse_recursive (head -> next);
+
+
+    printf ("%d ", head -> data);
+
+}
+
+ 
+
+void print (struct node *head)
+
+{
+
+    if (head == NULL)
+
+    {
+
+        return;
+
+    }
+
+    printf ("%d ", head -> data);
+
+    print (head -> next);
+
+}
+
+ 
+
+
+
+void insert_new_node (struct node ** head_ref, int new_data)
+
+{
+
+    struct node * new_node = (struct node *) malloc (sizeof (struct node));
+
+    new_node -> data = new_data;
+
+    new_node -> next = (*head_ref);
+
+    (*head_ref) = new_node;
+
+}
