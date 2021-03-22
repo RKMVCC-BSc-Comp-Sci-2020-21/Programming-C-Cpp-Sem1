b6abcecfabc5704816817014e40b913a1b913ef0
DIPMALYA HAZARI <75066464+DIP704@users.noreply.github.com>
2021-03-22 09:20:46 +0530

    Create conversion_between_centrigrade_farenhite.c

---
+++ conversion_between_centrigrade_farenhite.c
@@ -0,0 +1,16 @@
+//By Dipmalya Hazari
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int C=0,F=0;
+
+    printf("Enter the value in centrigade :",C);
+    scanf("%d",&C);
+
+    F= ((9*C)/5)+32;
+
+    printf("The value in farenhite: %d",F);
+    
+    return 0;
+}
